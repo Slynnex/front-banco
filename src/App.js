a86257a9486c8333ae7dfe80125bbc5c43f481c1
AlexNavarro <navarrov.alex@gmail.com>
2022-04-24 20:02:03 -0500

    Concepts CRUD listo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Manager from './pages/Manager';
 import Cashier from './pages/Cashier';
 
 import {Provider as ExecutiveProvider} from './context/Executives/ExecutivesContext'
+import {Provider as ConceptProvider} from './context/Concepts/ConceptsContext'
 
 function App() {
   return (
     <ExecutiveProvider>
+    <ConceptProvider>
       <Router>
         <Routes>
           <Route exact path='/' element={<Login/>}/>
@@ -17,6 +19,7 @@ function App() {
           <Route exact path='/cashier/*' element={<Cashier/>}/>
         </Routes>
       </Router>
+      </ConceptProvider>
     </ExecutiveProvider>
   );
 }
